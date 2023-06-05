75c9743c87678f97378dc7c287eae813de53cac7
Xincai Zhu <zxc021109@163.com>
2023-06-05 18:08:55 +0800

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+import subprocess
+import sys
+
+from LoginSession import LoginSession
+
+userId = "xxx"
+password = "xxx"
+if __name__ == "__main__":
+    x = subprocess.Popen("where tesseract", shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE, encoding='gbk').wait()
+    if x != 0:
+        print("Tesseract 尚未安装完善")
+        sys.exit(-1)
+    ex = LoginSession(userId=userId, password=password)
+    print(ex.transcript("新生实践课"))
